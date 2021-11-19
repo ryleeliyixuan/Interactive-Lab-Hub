aaaacd0ddca7ec8f58cda54a0d4c59d6743ed5cd
Yixuan Rylee Li <70308205+ryleeliyixuan@users.noreply.github.com>
2021-11-19 11:57:56 -0500

    Add files via upload

---
+++ Lab 6/senior-daily-care-checkin/checkin.py
@@ -0,0 +1,122 @@
+#the elderly's end
+
+import paho.mqtt.client as mqtt
+import uuid
+from subprocess import call
+from datetime import datetime
+import qwiic_button
+import time
+import pygame
+
+# Every client needs a random ID for log in 
+client = mqtt.Client(str(uuid.uuid1()))
+# configure network encryption etc
+client.tls_set()
+# this is the username and pw we have setup for the class
+client.username_pw_set('idd', 'device@theFarm')
+
+#connect to the broker
+client.connect(
+    'farlab.infosci.cornell.edu',
+    port=8883)
+
+
+p_button = qwiic_button.QwiicButton()
+if p_button.is_connected():
+    print("answer button detected!")
+
+pygame.mixer.init()
+pygame.mixer.music.load("./weather.wav")
+
+reported = False
+answered = False
+
+def check_time():
+    now = datetime.now()
+    current_time = now.strftime("%H")
+    # print("Current Time =", current_time)
+    attime = False
+    if current_time == "11":
+        attime = True
+
+    return attime
+
+# reset reported detail at 12 everyday
+def reset_time():
+    now = datetime.now()
+    current_time = now.strftime("%H")
+    # print("Current Time =", current_time)
+    attime = False
+    if current_time == "12":
+        attime = True
+
+    return attime
+
+
+def report():
+    reported = True
+    cmd_beg= 'espeak '
+    cmd_end= ' | aplay /home/pi/Desktop/Text.wav  2>/dev/null' # To play back the stored .wav file and to dump the std errors to /dev/null
+    cmd_out= '--stdout > /home/pi/Desktop/Text.wav ' # To store the voice file
+
+    text = "today weather is"
+
+    #Replacing ' ' with '_' to identify words in the text entered
+    text = text.replace(' ', '_')
+
+    #Calls the Espeak TTS Engine to read aloud a Text
+    call([cmd_beg+cmd_out+text+cmd_end], shell=True)
+
+    timer = time.time()
+
+#this is the callback that gets called once we connect to the broker.
+#we should add our subscribe functions here as well
+def on_connect(client, userdata, flags, rc):
+	print(f"connected with result code {rc}")
+	client.subscribe('IDD/checkup')
+	# you can subsribe to as many topics as you'd like
+	# client.subscribe('some/other/topic')
+
+
+# this is the callback that gets called each time a message is recived
+def on_message(cleint, userdata, msg):
+    print(f"topic: {msg.topic} msg: {msg.payload.decode('UTF-8')}")
+    print("message recieved")
+    if pygame.mixer.music.get_busy() == True:
+        print('playing music')
+    else:
+        pygame.mixer.music.play(1)
+        print("start to play music")
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+
+client.loop_start()
+while True:
+    # if recieve button pressed, send to MQTT
+    if p_button.is_button_pressed():
+        answered = True
+        val = "answered"
+        client.publish("IDD/checkin", val)
+
+    # reset every midnight
+    if reset_time():
+        answered = False
+        reported = False
+
+    #check if it is 9am
+    if check_time() and not reported:
+        # report()
+        reported = True
+        print("time to report")
+
+        if pygame.mixer.music.get_busy() == True:
+            print('playing music')
+            continue
+        else:
+            pygame.mixer.music.play(1)
+            print("start to play music")
+
+client.loop_stop()    #Stop loop
+client.disconnect()
---
+++ Lab 6/senior-daily-care-checkin/checkup.py
@@ -0,0 +1,113 @@
+#supervisor's end
+
+import paho.mqtt.client as mqtt
+import uuid
+import numpy as np
+
+import digitalio
+import board
+
+from adafruit_rgb_display.rgb import color565
+import adafruit_rgb_display.st7789 as st7789
+import webcolors
+from PIL import Image, ImageDraw, ImageFont
+
+import qwiic_button
+
+# ----------------
+# enable buttons on adafruit adafruit_rgb_display
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+BAUDRATE = 64000000  # the rate  the screen talks to the pi
+# Create the ST7789 display:
+display = st7789.ST7789(
+    board.SPI(),
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+height = display.width
+width = display.height
+
+# these setup the code for our buttons and the backlight and tell the pi to treat the GPIO pins as digitalIO vs analogIO
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+display.fill(color565(0, 255, 0))
+
+
+image = Image.new("RGB", (width, height))
+rotation = 90
+
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+font_big = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 32)
+
+
+
+# some other examples
+topic = 'IDD/checkin'
+
+#this is the callback that gets called once we connect to the broker.
+#we should add our subscribe functions here as well
+def on_connect(client, userdata, flags, rc):
+	print(f"connected with result code {rc}")
+	client.subscribe(topic)
+	# you can subsribe to as many topics as you'd like
+	# client.subscribe('some/other/topic')
+
+
+# this is the callback that gets called each time a message is recived
+def on_message(cleint, userdata, msg):
+    print(f"topic: {msg.topic} msg: {msg.payload.decode('UTF-8')}")
+    draw = ImageDraw.Draw(image)
+    print("message recieved")
+    draw.text((0.5, 10), "check in recieved", font=font, fill="#FFFFFF")
+    display.image(image, rotation)
+
+
+# Every client needs a random ID
+client = mqtt.Client(str(uuid.uuid1()))
+# configure network encryption etc
+client.tls_set()
+# this is the username and pw we have setup for the class
+client.username_pw_set('idd', 'device@theFarm')
+
+# attach out callbacks to the client
+client.on_connect = on_connect
+client.on_message = on_message
+
+#connect to the broker
+client.connect(
+    'farlab.infosci.cornell.edu',
+    port=8883)
+
+
+p_button = qwiic_button.QwiicButton()
+if p_button.is_connected():
+    print("answer button detected!")
+client.loop_start()
+while True:
+    if p_button.is_button_pressed():
+
+        val = "answered"
+        client.publish("IDD/checkup", "inquiry")
+
+client.loop_stop()    #Stop loop
+client.disconnect()
+
+
+# this is blocking. to see other ways of dealing with the loop
+#  https://www.eclipse.org/paho/index.php?page=clients/python/docs/index.php#network-loop
+# client.loop_forever()
